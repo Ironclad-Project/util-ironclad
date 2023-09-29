7c5c030cb8d31c81d34b5e0c409c20d2251fd8b2
streaksu <streaksu@protonmail.com>
2023-09-28 19:02:22 -0500

    update strace and ps

--- source/ps.c
+++ source/ps.c
@@ -25,8 +25,13 @@
 #include <entrypoints.h>
 
 #define SC_LIST_PROCS   8
+#define SC_LIST_THREADS 12
+#define SC_LIST_CLUSTERS 13
 #define PROC_IS_TRACED  0b01
 #define PROC_EXITED     0b10
+#define SCHED_RR   0b001
+#define SCHED_COOP 0b010
+#define SCHED_INTR 0b100
 
 struct procinfo {
     char     id[20];
@@ -37,11 +42,26 @@ struct procinfo {
     uint32_t flags;
 } __attribute__((packed));
 
+
+struct threadinfo {
+    uint16_t tid;
+    uint16_t tcid;
+    uint16_t pid;
+} __attribute__((packed));
+
+struct tclusterinfo {
+    uint16_t tcid;
+    uint16_t tcflags;
+    uint16_t tcquantum;
+} __attribute__((packed));
+
 int ps_entrypoint(int argc, char *argv[]) {
     int print_all_users = 0;
+    int print_threads   = 0;
+    int print_clusters  = 0;
     int print_running   = 0;
     char c;
-    while ((c = getopt (argc, argv, "hAr")) != -1) {
+    while ((c = getopt (argc, argv, "hATCr")) != -1) {
         switch (c) {
             case 'h':
                 puts("Usage: ps [options]");
@@ -49,10 +69,14 @@ int ps_entrypoint(int argc, char *argv[]) {
                 puts("Options:");
                 puts("-h             Print this help message");
                 puts("-A             Print all processes regardless of user");
+                puts("-T             Print threads instead of processes");
+                puts("-C             Print thread clusters instead of processes");
                 puts("-r             Print running ones only");
                 puts("-v | --version Display version information.");
                 return 0;
             case 'A': print_all_users = 1; break;
+            case 'T': print_threads   = 1; break;
+            case 'C': print_clusters  = 1; break;
             case 'r': print_running   = 1; break;
             default:
                 fprintf(stderr, "ps: Unknown option '%c'\n", optopt);
@@ -60,37 +84,98 @@ int ps_entrypoint(int argc, char *argv[]) {
         }
     }
 
-    struct procinfo *buffer = malloc(50 * sizeof(struct procinfo));
+    if (print_threads) {
+        struct threadinfo *buffer = malloc(50 * sizeof(struct threadinfo));
 
-    long ret, errno;
-    SYSCALL3(SYSCALL_SYSCONF, SC_LIST_PROCS, buffer, 50 * sizeof(struct procinfo));
-    if (ret == -1) {
-        return 1;
-    } else if (ret > 50) {
-        return 1;
+        long ret, errno;
+        SYSCALL3(SYSCALL_SYSCONF, SC_LIST_THREADS, buffer, 50 * sizeof(struct procinfo));
+        if (ret == -1) {
+            return 1;
+        } else if (ret > 50) {
+            return 1;
+        }
+        long ret2 = ret;
+
+        struct procinfo *buffer2 = malloc(50 * sizeof(struct procinfo));
+        SYSCALL3(SYSCALL_SYSCONF, SC_LIST_PROCS, buffer2, 50 * sizeof(struct procinfo));
+        if (ret == -1) {
+            return 1;
+        } else if (ret > 50) {
+            return 1;
+        }
+
+        printf("%4s %4s %4s %20s\n", "TID", "TCID", "PID", "CMD");
+        for (int i = 0; i < ret2; i++) {
+            int found_idx = 0;
+            for (int j = 0; j < ret; j++) {
+                if (buffer2[j].pid == buffer[i].pid) {
+                    found_idx = j;
+                    break;
+                }
+            }
+            printf("%4d %4d ", buffer[i].tid, buffer[i].tcid);
+            printf("%4d %20.*s\n", buffer[i].pid,
+               buffer2[found_idx].id_len, buffer2[found_idx].id);
+        }
+    } else if (print_clusters) {
+        struct tclusterinfo *buffer = malloc(50 * sizeof(struct tclusterinfo));
+
+        long ret, errno;
+        SYSCALL3(SYSCALL_SYSCONF, SC_LIST_CLUSTERS, buffer, 50 * sizeof(struct tclusterinfo));
+        if (ret == -1) {
+            return 1;
+        } else if (ret > 50) {
+            return 1;
+        }
+
+        printf("%4s %7s %4s\n", "TCID", "ALGO(I)", "QTUM");
+        for (int i = 0; i < ret; i++) {
+            printf("%4d ", buffer[i].tcid);
+            if (buffer[i].tcflags & SCHED_RR) {
+                printf("  RR");
+            } else if (buffer[i].tcflags & SCHED_COOP) {
+                printf("COOP");
+            }
+            if (buffer[i].tcflags & SCHED_INTR) {
+                printf("(*)");
+            } else {
+                printf("   ");
+            }
+            printf(" %4d\n", buffer[i].tcquantum);
+        }
+    } else {
+        struct procinfo *buffer = malloc(50 * sizeof(struct procinfo));
+
+        long ret, errno;
+        SYSCALL3(SYSCALL_SYSCONF, SC_LIST_PROCS, buffer, 50 * sizeof(struct procinfo));
+        if (ret == -1) {
+            return 1;
+        } else if (ret > 50) {
+            return 1;
+        }
+
+        uid_t current_uid = getuid();
+
+        printf("%4s %4s %11s %20s\n", "PPID", "PID", "STAT", "CMD");
+        for (int i = 0; i < ret; i++) {
+            char flags_message[] = "xxx-xxx";
+            if (buffer[i].flags & PROC_IS_TRACED) {
+                flags_message[0] = 't';
+                flags_message[1] = 'r';
+                flags_message[2] = 'a';
+            }
+            if (buffer[i].flags & PROC_EXITED) {
+                flags_message[4] = 'e';
+                flags_message[5] = 'x';
+                flags_message[6] = 'd';
+            }
+            if ((print_all_users || buffer[i].uid == current_uid) &&
+                (!print_running || (buffer[i].flags & PROC_EXITED) == 0)) {
+                printf("%4d %4d %11s %20.*s\n", buffer[i].ppid, buffer[i].pid,
+                       flags_message, buffer[i].id_len, buffer[i].id);
+            }
+        }
     }
 
-    uid_t current_uid = getuid();
-
-    printf("%4s %4s %11s %20s\n", "PPID", "PID", "STAT", "CMD");
-    for (int i = 0; i < ret; i++) {
-        char flags_message[] = "xxx-xxx";
-        if (buffer[i].flags & PROC_IS_TRACED) {
-            flags_message[0] = 't';
-            flags_message[1] = 'r';
-            flags_message[2] = 'a';
-        }
-        if (buffer[i].flags & PROC_EXITED) {
-            flags_message[4] = 'e';
-            flags_message[5] = 'x';
-            flags_message[6] = 'd';
-        }
-        if ((print_all_users || buffer[i].uid == current_uid) &&
-            (!print_running || (buffer[i].flags & PROC_EXITED) == 0)) {
-            printf("%4d %4d %11s %20.*s\n", buffer[i].ppid, buffer[i].pid,
-                   flags_message, buffer[i].id_len, buffer[i].id);
-        }
-    }
-
-   return 0;
+    return 0;
 }
--- source/strace.c
+++ source/strace.c
@@ -68,7 +68,7 @@ static void print_syscall(FILE *out, struct registers state) {
             break;
         case SYSCALL_OPEN:
             fprintf(out, "open(0x%lx, 0x%lx, 0x%lx, 0x%lx)", state.rdi,
-                    state.rsi, state.rdx, state.rcx);
+                    state.rsi, state.rdx, state.r12);
             break;
         case SYSCALL_CLOSE:
             fprintf(out, "close(0x%lx)", state.rdi);
@@ -87,7 +87,7 @@ static void print_syscall(FILE *out, struct registers state) {
             break;
         case SYSCALL_MMAP:
             fprintf(out, "mmap(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
-                    state.rdi, state.rsi, state.rdx, state.rcx, state.r8,
+                    state.rdi, state.rsi, state.rdx, state.r12, state.r8,
                     state.r9);
             break;
         case SYSCALL_MUNMAP:
@@ -101,41 +101,224 @@ static void print_syscall(FILE *out, struct registers state) {
             break;
         case SYSCALL_EXEC:
             fprintf(out, "exec(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)\n",
-                    state.rdi, state.rsi, state.rdx, state.rcx, state.r8,
+                    state.rdi, state.rsi, state.rdx, state.r12, state.r8,
                     state.r9);
             break;
         case SYSCALL_CLONE:
-            fprintf(out, "clone(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
-                    state.rdi, state.rsi, state.rdx, state.rcx, state.r8);
+            fprintf(out, "clone(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12, state.r8, state.r9);
             break;
         case SYSCALL_WAIT:
             fprintf(out, "wait(0x%lx, 0x%lx, 0x%lx)",
                     state.rdi, state.rsi, state.rdx);
             break;
+        case SYSCALL_SOCKET:
+            fprintf(out, "socket(0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx);
+            break;
+        case SYSCALL_SETHOSTNAME:
+            fprintf(out, "sethostname(0x%lx, 0x%lx)", state.rdi, state.rsi);
+            break;
+        case SYSCALL_STAT:
+            fprintf(out, "stat(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12, state.r8);
+            break;
+        case SYSCALL_CHDIR:
+            fprintf(out, "chdir(0x%lx)", state.rdi);
+            break;
         case SYSCALL_IOCTL:
             fprintf(out, "ioctl(0x%lx, 0x%lx, 0x%lx)", state.rdi, state.rsi,
                     state.rdx);
             break;
-        case SYSCALL_GETDENTS:
-            fprintf(out, "getdents(0x%lx, 0x%lx, 0x%lx)", state.rdi,
-                    state.rsi, state.rdx);
-            break;
-        case SYSCALL_STAT:
-            fprintf(out, "stat(0x%lx, 0x%lx)", state.rdi, state.rsi);
-            break;
         case SYSCALL_SCHED_YIELD:
             fprintf(out, "sched_yield()");
+            break;
+        case SYSCALL_DELETE_TCLUSTER:
+            fprintf(out, "delete_tcluster(0x%lx)", state.rdi);
             break;
         case SYSCALL_PIPE:
             fprintf(out, "pipe(0x%lx, 0x%lx)", state.rdi, state.rsi);
             break;
+        case SYSCALL_GETUID:
+            fprintf(out, "getuid()");
+            break;
+        case SYSCALL_RENAME:
+            fprintf(out, "rename(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12, state.r8,
+                    state.r9, state.r10);
+            break;
+        case SYSCALL_SYSCONF:
+            fprintf(out, "sysconf(0x%lx, 0x%lx, 0x%lx)", state.rdi, state.rsi,
+                    state.rdx);
+            break;
+        case SYSCALL_SPAWN:
+            fprintf(out, "spawn(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12, state.r8,
+                    state.r9, state.r10);
+            break;
+        case SYSCALL_GETTID:
+            fprintf(out, "gettid()");
+            break;
+        case SYSCALL_MANAGE_TCLUSTER:
+            fprintf(out, "manage_tcluster(0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12);
+            break;
+        case SYSCALL_FCNTL:
+            fprintf(out, "fcntl(0x%lx, 0x%lx, 0x%lx)", state.rdi, state.rsi,
+                    state.rdx);
+            break;
+        case SYSCALL_GETRANDOM:
+            fprintf(out, "getrandom(0x%lx, 0x%lx)", state.rdi, state.rsi);
+            break;
+        case SYSCALL_MPROTECT:
+            fprintf(out, "mprotect(0x%lx, 0x%lx, 0x%lx)", state.rdi, state.rsi,
+                    state.rdx);
+            break;
+        case SYSCALL_SET_MAC_CAPABILITIES:
+            fprintf(out, "set_mac_caps(0x%lx)", state.rdi);
+            break;
+        case SYSCALL_GET_MAC_CAPABILITIES:
+            fprintf(out, "get_mac_caps()");
+            break;
+        case SYSCALL_ADD_MAC_PERMISSIONS:
+            fprintf(out, "add_mac_perms(0x%lx, 0x%lx, 0x%lx)", state.rdi, state.rsi,
+                    state.rdx);
+            break;
+        case SYSCALL_SET_MAC_ENFORCEMENT:
+            fprintf(out, "set_mac_enforcement(0x%lx)", state.rdi);
+            break;
+        case SYSCALL_MOUNT:
+            fprintf(out, "mount(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12, state.r8, state.r9);
+            break;
+        case SYSCALL_UMOUNT:
+            fprintf(out, "umount(0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx);
+            break;
+        case SYSCALL_READLINK:
+            fprintf(out, "readlink(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12, state.r8);
+            break;
+        case SYSCALL_GETDENTS:
+            fprintf(out, "getdents(0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx);
+            break;
+        case SYSCALL_SYNC:
+            fprintf(out, "sync()");
+            break;
+        case SYSCALL_MAKENODE:
+            fprintf(out, "makenode(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12, state.r8);
+            break;
         case SYSCALL_UNLINK:
-            fprintf(out, "unlink(0x%lx, 0x%lx, 0x%lx)", state.rdi, state.rsi,
-                         state.rdx);
+            fprintf(out, "unlink(0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx);
+            break;
+        case SYSCALL_TRUNCATE:
+            fprintf(out, "truncate(0x%lx, 0x%lx)", state.rdi, state.rsi);
+            break;
+        case SYSCALL_BIND:
+            fprintf(out, "bind(0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx);
+            break;
+        case SYSCALL_SYMLINK:
+            fprintf(out, "symlink(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12, state.r8, state.r9);
+            break;
+        case SYSCALL_CONNECT:
+            fprintf(out, "connect(0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx);
+            break;
+        case SYSCALL_OPENPTY:
+            fprintf(out, "openpty(0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx);
+            break;
+        case SYSCALL_FSYNC:
+            fprintf(out, "fsync(0x%lx, 0x%lx)", state.rdi, state.rsi);
+            break;
+        case SYSCALL_LINK:
+            fprintf(out, "link(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12, state.r8, state.r9);
+            break;
+        case SYSCALL_PTRACE:
+            fprintf(out, "ptrace(0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12);
+            break;
+        case SYSCALL_LISTEN:
+            fprintf(out, "listen(0x%lx, 0x%lx)", state.rdi, state.rsi);
+            break;
+        case SYSCALL_ACCEPT:
+            fprintf(out, "accept(0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12);
+            break;
+        case SYSCALL_GETRLIMIT:
+            fprintf(out, "getrlimit(0x%lx)", state.rdi);
+            break;
+        case SYSCALL_SETRLIMIT:
+            fprintf(out, "setrlimit(0x%lx, 0x%lx)", state.rdi, state.rsi);
+            break;
+        case SYSCALL_ACCESS:
+            fprintf(out, "access(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12, state.r8);
+            break;
+        case SYSCALL_POLL:
+            fprintf(out, "poll(0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx);
+            break;
+        case SYSCALL_GETEUID:
+            fprintf(out, "geteuid()");
+            break;
+        case SYSCALL_SETUIDS:
+            fprintf(out, "setuids(0x%lx, 0x%lx)", state.rdi, state.rsi);
+            break;
+        case SYSCALL_FCHMOD:
+            fprintf(out, "chmod(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12, state.r8);
+            break;
+        case SYSCALL_UMASK:
+            fprintf(out, "umask(0x%lx)", state.rdi);
+            break;
+        case SYSCALL_REBOOT:
+            fprintf(out, "reboot(0x%lx, 0x%lx)", state.rdi, state.rsi);
+            break;
+        case SYSCALL_FCHOWN:
+            fprintf(out, "chown(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12, state.r8);
+            break;
+        case SYSCALL_PREAD:
+            fprintf(out, "pread(0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12);
+            break;
+        case SYSCALL_PWRITE:
+            fprintf(out, "pwrite(0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12);
+            break;
+        case SYSCALL_GETSOCKNAME:
+            fprintf(out, "getsockname(0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx);
+            break;
+        case SYSCALL_GETPEERNAME:
+            fprintf(out, "getpeername(0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx);
+            break;
+        case SYSCALL_SHUTDOWN:
+            fprintf(out, "shutdown(0x%lx, 0x%lx)", state.rdi, state.rsi);
+            break;
+        case SYSCALL_FUTEX:
+            fprintf(out, "futex(0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12);
+            break;
+        case SYSCALL_CLOCK:
+            fprintf(out, "clock(0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx);
+            break;
+        case SYSCALL_CLOCK_NANOSLEEP:
+            fprintf(out, "clock_nanosleep(0x%lx, 0x%lx, 0x%lx, 0x%lx)",
+                    state.rdi, state.rsi, state.rdx, state.r12);
             break;
         default:
             fprintf(out, "(%lu)(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)",
-                    state.rax, state.rdi, state.rsi, state.rdx, state.rcx,
+                    state.rax, state.rdi, state.rsi, state.rdx, state.r12,
                     state.r8, state.r9);
             break;
     }
@@ -201,6 +384,7 @@ END_WHILE:
     }
 
     int ret, errno, status;
+    uint16_t thread_id;
     struct registers state;
 
     SYSCALL4(SYSCALL_PTRACE, 1, child, 0, pipes[1]);
@@ -226,8 +410,10 @@ END_WHILE:
            return 1;
         }
         if (polled.revents & POLLIN) {
+           read(pipes[0], &thread_id, sizeof(uint16_t));
            if (read(pipes[0], &state, sizeof(state)) == sizeof(state)) {
                if (is_syscall == true) {
+                   fprintf(out, "%d: ", thread_id);
                    print_syscall(out, state);
                    is_syscall = state.rax == SYSCALL_EXIT        ||
                                 state.rax == SYSCALL_EXIT_THREAD ||
