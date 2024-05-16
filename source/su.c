9ba85fa06bd1a13af544c200448b28c3f9199681
streaksu <streaksu@mailbox.org>
2024-05-16 11:40:20 +0200

    su: add -c flag
    Signed-off-by: streaksu <streaksu@mailbox.org>

--- source/su.c
+++ source/su.c
@@ -27,20 +27,25 @@
 int main(int argc, char *argv[]) {
     int do_login_shell = 0;
     char *user = NULL;
+    char **command = NULL;
 
     for (int i = 1; i < argc; i++) {
-        if (!strcmp(argv[i], "-hv")) {
-            puts("Usage: su [options] [-] [username]");
+        if (!strcmp(argv[i], "-h")) {
+            puts("Usage: su [options] [-] [username] [-c \"<command>\"]");
             puts("");
             puts("Options:");
-            puts("-h              Print this help message");
-            puts("-v              Display version information.");
+            puts("-h               Print this help message");
+            puts("-v               Display version information.");
+            puts("-c \"<command>\" Instead of logging, execute a command in the shell");
             return 0;
         } else if (!strcmp(argv[i], "-v")) {
             puts("su" VERSION_STR);
             return 0;
         } else if (!strcmp(argv[i], "-")) {
             do_login_shell = 1;
+        } else if (!strcmp(argv[i], "-c")) {
+            command = &argv[i + 1];
+            break;
         } else {
             user = argv[i];
             break;
@@ -60,7 +65,26 @@ int main(int argc, char *argv[]) {
     setuid(pwd->pw_uid);
     seteuid(pwd->pw_uid);
 
-    if (do_login_shell) {
+    if (command != NULL) {
+        size_t arg_count;
+        for (arg_count = 0; command[arg_count] != NULL; arg_count += 1);
+
+        // 1 more for pw_shell, 1 more for -c, 1 more for NULL.
+        char **new_argvp = malloc(sizeof(char *) * (arg_count + 3));
+        if (new_argvp == NULL) {
+            perror("su: could not allocate new argvp string");
+            return 1;
+        }
+
+        new_argvp[0] = strdup(pwd->pw_shell);
+        new_argvp[1] = strdup("-c");
+        for (size_t i = 0; i < arg_count; i++) {
+            new_argvp[i + 2] = strdup(command[i]);
+        }
+        new_argvp[arg_count + 2] = NULL;
+
+        execvp(pwd->pw_shell, new_argvp);
+    } else if (do_login_shell) {
         setenv("HOME", pwd->pw_dir, 1);
         execl(pwd->pw_shell, pwd->pw_shell, "--login", NULL);
     } else {
